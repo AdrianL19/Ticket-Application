6828d7c0872405ff25709b56d1aa216304c0804d
Adrian Borca <adrianborca99@yahoo.com>
2020-12-02 13:37:59 +0200

    Administrator View

--- src/main/java/com/example/application/views/User/RoutesView.java
+++ src/main/java/com/example/application/views/User/RoutesView.java
@@ -1,14 +1,14 @@
-package com.example.application.views.Routes;
+package com.example.application.views.User;
 
+import com.example.application.views.main.MainViewUser;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.example.application.views.main.MainViewUser;
 
 @Route(value = "routes", layout = MainViewUser.class)
-@PageTitle("About")
-public class RoutesView extends Div {
+@PageTitle("Routes")
+public  class RoutesView extends Div {
 
     public RoutesView() {
         setId("about-view");
