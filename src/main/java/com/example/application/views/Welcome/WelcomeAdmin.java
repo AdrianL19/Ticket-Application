c31de74007fb2c054a2b31a228b4e6c27c2f5438
Adrian Borca <adrianborca99@yahoo.com>
2020-12-02 13:02:15 +0200

    Administrator View

--- src/main/java/com/example/application/views/Welcome/WelcomeAdmin.java
+++ src/main/java/com/example/application/views/Welcome/WelcomeAdmin.java
@@ -4,22 +4,27 @@ import com.vaadin.flow.component.html.Div;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.example.application.views.main.MainViewUser;
 
+
 @Route(value = "welcomeAdmin", layout = MainViewUser.class)
 @PageTitle("Admin Welcome")
 public class WelcomeAdmin extends VerticalLayout {
-
+    private H1 title = new H1("Welcome Administrator!");
+    private H4 redirectUser = new H4("If you want to add/edit/delete any information about an user press the button below.");
     public WelcomeAdmin() {
         setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
         setAlignItems(FlexComponent.Alignment.CENTER);
         setId("about-view");
 
-        add(new Text("Welcome Administrator!"));
+        setSpacing(false);
+        add(title,redirectUser);
     }
 
 }
